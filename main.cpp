c60f0c99b82a3046b655d43cd38312d0a8ef3aff
bakr2023-2024 <bakr2023.2024@gmail.com>
2026-05-09 11:53:10 +0300

    feat: draw outline of maurer rose

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ int main()
     InitWindow(sw, sh, "Program");
     int n = 7, d = 29;
     Vector2 *points = new Vector2[361];
+    Vector2 *outlines = new Vector2[361];
     Vector2 origin = Vector2{sw / 2.0f, sh / 2.0f};
     points[0] = origin;
     for (int i = 1; i <= 360; i++)
@@ -15,6 +16,9 @@ int main()
         float k1 = i * d * M_PI / 180;
         float r1 = 300 * sinf(n * k1);
         points[i] = Vector2{r1 * cosf(k1) + origin.x, r1 * sinf(k1) + origin.y};
+        float k2 = i * M_PI / 180;
+        float r2 = 300 * sinf(n * k2);
+        outlines[i] = Vector2{r2 * cosf(k2) + origin.x, r2 * sinf(k2) + origin.y};
     }
     bool outlinesOn = false;
     while (!WindowShouldClose())
@@ -22,9 +26,11 @@ int main()
         BeginDrawing();
         ClearBackground(BLACK);
         DrawLineStrip(points, 361, BLUE);
+        DrawSplineLinear(outlines, 361, 4.0f, RED);
         EndDrawing();
     }
     delete[] points;
+    delete[] outlines;
     CloseWindow();
     return 0;
 }
